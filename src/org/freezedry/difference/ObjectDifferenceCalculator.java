20a72a26c13f2aba11137904ea40b6001b06a5b8
Rob Philipp <rphilipp@investlab.com>
2013-10-07 19:57:06 -0400

    added timing to the tests

--- src/org/freezedry/difference/ObjectDifferenceCalculator.java
+++ src/org/freezedry/difference/ObjectDifferenceCalculator.java
@@ -325,13 +325,62 @@ public class ObjectDifferenceCalculator {
 		personMap2.put( "pretty", new Person( "Ginder", "Mendez", 23 ) );
 		division2.setPersonMap( personMap2 );
 
-
+		//
+		System.out.println( "Complex Object" );
+		long start = System.currentTimeMillis();
 		final ObjectDifferenceCalculator diffCalc = new ObjectDifferenceCalculator( "." );
-		final Map< String, Difference > difference = diffCalc.calculateDifference( division, division2 );
+		Map< String, Difference > difference = null;
+		long iters = 1_000;
+		for( int i = 0; i < iters; ++i )
+		{
+			difference = diffCalc.calculateDifference( division, division2 );
+		}
 		for( Map.Entry< String, Difference > entry : difference.entrySet() )
 		{
 			System.out.println( entry.getKey() + ": " + entry.getValue().toString() );
 		}
+		System.out.println( (double)( System.currentTimeMillis() - start ) / iters + " ms per comparison");
+
+		//
+		System.out.println( "Simple Object" );
+		Account account1 = new Account( 12345L, "firmABCD", "office123", "12345", "cash", "individual", "USD", "US", true, Calendar.getInstance().getTime() );
+		Account account2 = new Account( 12345L, "firmABC", "office123", "12345", "cash", "individual", "USD", "US", true, Calendar.getInstance().getTime() );
+		start = System.currentTimeMillis();
+		for( int i = 0; i < iters; ++i )
+		{
+			difference = diffCalc.calculateDifference( account1, account2 );
+		}
+		for( Map.Entry< String, Difference > entry : difference.entrySet() )
+		{
+			System.out.println( entry.getKey() + ": " + entry.getValue().toString() );
+		}
+		System.out.println( (double)( System.currentTimeMillis() - start ) / iters + " ms per comparison");
 	}
 
+	static class Account {
+		private Long accountId;
+		private String firmCode;
+		private String officeCode;
+		private String accountNumber;
+		private String accountType;
+		private String tradingType;
+		private String baseCurrency;
+		private String countryCode;
+		private Boolean accountActive;
+		private Date creationDate;
+
+		Account( Long accountId, String firmCode, String officeCode, String accountNumber, String accountType, String tradingType, String baseCurrency, String countryCode, Boolean accountActive, Date creationDate )
+		{
+			this.accountId = accountId;
+			this.firmCode = firmCode;
+			this.officeCode = officeCode;
+			this.accountNumber = accountNumber;
+			this.accountType = accountType;
+			this.tradingType = tradingType;
+			this.baseCurrency = baseCurrency;
+			this.countryCode = countryCode;
+			this.accountActive = accountActive;
+			this.creationDate = creationDate;
+		}
+	}
 }
